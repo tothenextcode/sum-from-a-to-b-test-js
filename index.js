52b9954f27fe8031b4b1c6bbc5f34d13ea230312
JC Flores <tothenextcode@gmail.com>
2022-04-08 13:44:32 +0000

    Implemented sumAtoBRecursively and gitignored package-lock.json

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if (fromN == toN) {
+    return toN;
+  }
+
+  return fromN + sum(fromN + 1, toN)
 }
+
+sum(3, 7);
 
 module.exports = sum;
